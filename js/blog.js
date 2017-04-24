409c1dcd90470bbc7346be5ee58b80bc03b03e10
nanomosfet <timmyzsearcy@gmail.com>
2017-04-24 09:25:56 -0700

    updates to layout

--- js/blog.js
+++ js/blog.js
@@ -5,9 +5,9 @@ function likeRequest(loop, blog_id) {
 	var idx = "blog_idx=" + loop;
 	var params = id + "&" + idx;
 	http_request.onreadystatechange = function() {
-		if (this.readyState == 4 && this.status == 200) {				
+		if (this.readyState == 4 && this.status == 200) {
 			var json_obj = JSON.parse(this.responseText);
-			error_element = document.getElementById("error-"+String(loop));			
+			error_element = document.getElementById("error-"+String(loop));
 			error_element.style.display = 'block';
 			error_element.style.opacity = 1;
 			error_element.innerHTML = json_obj.error
@@ -42,7 +42,7 @@ function fade(element) {
         op -= op * 0.1;
     }, 50);
 }
-	
+
 function unfade(element) {
     var op = 0.1;  // initial opacity
     element.style.display = 'block';
@@ -58,10 +58,11 @@ function unfade(element) {
 
 function commentRequest(blog_id) {
 	var http_request = new XMLHttpRequest();
-	var id = "blog_id=" + blog_id;
 	var new_comment =  document.getElementById("new-comment").value;
-	var comment_text = "comment_text=" + new_comment;
-	var params = id + "&" + comment_text;
+	var form = new FormData();
+	form.append('blog_id', blog_id);
+	form.append('comment_text',new_comment);
+	form.append('add_comment', true);
 	var comment_section = document.getElementById("existing-comments");
 	var error_element = document.getElementById("error");
 	error_element.style.display = 'block';
@@ -75,19 +76,27 @@ function commentRequest(blog_id) {
 				var hr = document.createElement("hr");
 				var p = document.createElement("pre");
 				var b = document.createElement("button");
-				var t = document.createTextNode("Delete");
+				var t = document.createTextNode("Delete Comment");
+				var e = document.createElement("button")
+				var t_e = document.createTextNode("Edit")
+				e.id = "edit-comment-" + json_obj.comment_id;
+				e.appendChild(t_e);
+				console.log("In commentRequest: " + json_obj.comment_id);
+				e.onclick = function() { toggle_edit_comment(json_obj.comment_id, new_comment) };
 				b.onclick = function() { delete_comment(json_obj.comment_id, blog_id) };
 				b.appendChild(t)
-				d.id = "comment-" + json_obj.comment_id;				
+				d.id = "comment-" + json_obj.comment_id;
+				d.appendChild(document.createTextNode(""));
 				p.appendChild(c);
 				d.appendChild(p);
 				d.appendChild(b)
+				d.appendChild(e);
 				d.appendChild(hr);
-				
+
 
 				comment_section.appendChild(d);
 				error_element.innerHTML = json_obj.error
-			} else { 
+			} else {
 				error_element.innerHTML = json_obj.error
 			}
 			setTimeout(function(){
@@ -99,9 +108,9 @@ function commentRequest(blog_id) {
 
 	}
 	http_request.open("POST", "/comments", true);
-	http_request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-	console.log(params);
-	http_request.send(params);
+	//http_request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	console.log(form);
+	http_request.send(form);
 }
 
 function delete_comment(comment_id, blog_id) {
@@ -110,7 +119,7 @@ function delete_comment(comment_id, blog_id) {
 	var delete_comment = "delete_comment=true"
 	var blog_id = "blog_id=" + blog_id;
 	var comment_element = document.getElementById("comment-" + comment_id)
-	
+
 	var params = id + "&" + delete_comment + "&" + blog_id;
 	http_request.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
@@ -123,3 +132,133 @@ function delete_comment(comment_id, blog_id) {
 	console.log(params);
 	http_request.send(params);
 }
+
+
+
+
+var edit_comment_mode = false;
+
+function toggle_edit_comment(comment_id, comment_content) {
+	var comment_area = document.getElementById("new-comment");
+	var submit_button = document.getElementById("submit-comment");
+	var cancel_button = document.createElement("button");
+	var edit_button = document.getElementById("edit-comment-" + comment_id);
+	if (edit_comment_mode == true) {
+		return 0;
+	}
+	cancel_text = document.createTextNode("Cancel")
+	cancel_button.appendChild(cancel_text);
+	submit_button.insertAdjacentElement("afterend", cancel_button);
+	var old_submit_onclick = submit_button.onclick
+	comment_area.value = comment_content;
+	submit_button.onclick = function() {
+		comment_content = comment_area.value;
+		console.log("In toggle_edit_comment: " + comment_id);
+		update_comment(comment_id, comment_content);
+		cancel_button.parentNode.removeChild(cancel_button);
+		submit_button.onclick = old_submit_onclick;
+		edit_comment_mode = false;
+
+	};
+
+
+
+
+	cancel_button.onclick = function() {
+		comment_area.value = "";
+		submit_button.onclick = old_submit_onclick;
+		cancel_button.parentNode.removeChild(cancel_button);
+		edit_comment_mode = false;
+
+	};
+	edit_comment_mode = true;
+
+//TODO: create update_comment
+function update_comment(comment_id, comment_content) {
+	var http_request = new XMLHttpRequest();
+	var error_element = document.getElementById("error");
+	var edit_button = document.getElementById("edit-comment-" + comment_id);
+	form = new FormData();
+	form.append("update_comment", true);
+	form.append("comment_id", comment_id);
+	form.append("comment_text", comment_content);
+
+	http_request.onreadystatechange = function() {
+		if(this.readyState == 4 && this.status == 200) {
+			json_obj = JSON.parse(this.responseText);
+			if(json_obj.ret_val == true) {
+				comment_area.value = "";
+				comment = document.getElementById("comment-" + json_obj.comment_id).firstChild.nextSibling;
+				comment.innerHTML = json_obj.user + ": " + json_obj.comment_content;
+				
+				// edit_button.onclick = function() {
+				// 	console.log("In update: " + json_obj.comment_id);
+				// 	toggle_edit_comment(json_obj.comment_id, json_obj.comment_content);
+				// };
+
+			}
+			error_element.innerHTML = json_obj.error;
+			setTimeout(function(){
+				fade(error_element);
+			},2000);
+		}
+
+	};
+	http_request.open("POST", "/comments", true);
+	http_request.send(form)
+
+}
+
+}
+
+function upload_image(blog_id) {
+	var http_request = new XMLHttpRequest();
+	var img_upload_element = document.getElementById("img");
+	var formData = new FormData();
+
+	formData.append("blog_id", blog_id);
+	formData.append("img", img_upload_element.files[0]);
+	var img_element = document.getElementById("blog-img");
+	var blog_body = document.getElementById("blog-body")
+
+
+	http_request.onreadystatechange = function() {
+		
+		if (this.readyState == 4 && this.status == 200) {
+			var rand_num = Math.floor((Math.random()*10000) + 1)
+			if (img_element == null) {
+				img_element = document.createElement("img");
+				img_element.id = "blog-img";
+				img_element.src = "/img?img_id="+String(blog_id) +"&n=" + rand_num;
+				blog_body.append(img_element);
+			} else {				
+				img_element.src = "/img?img_id="+String(blog_id) +"&n=" + rand_num;
+			}
+		}
+
+	}
+
+	http_request.open("POST", "/images", true);
+	//http_request.setRequestHeader("Content-type", "multipart/form-data");
+	console.log(formData)
+	http_request.send(formData);
+
+}
+
+function delete_image(blog_id) {
+		var i = document.getElementById("blog-img");
+		var http_request = new XMLHttpRequest();
+		var form = new FormData();
+		i.parentNode.removeChild(i);
+		http_request.onreadystatechange = function() {
+			if (this.readyState == 4 && self.status == 200) {
+				
+			}
+		};
+		
+		form.append("delete_image", true);
+		form.append("blog_id", blog_id);
+
+		http_request.open("POST", "/images");
+		http_request.send(form);
+	}
--- main.py
+++ main.py
@@ -11,12 +11,14 @@ import cookieStuff
 import hashpass
 import json
 
-
-from google.appengine.ext import db
+from google.appengine.api import images
+from google.appengine.ext import ndb as db
 
 template_dir = os.path.join(os.path.dirname(__file__), "templates")
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
 											autoescape = True)
+
+
 
 
 class Handler(webapp2.RequestHandler):
@@ -33,15 +35,48 @@ class Handler(webapp2.RequestHandler):
 	def user_set(self, redirect = True):
 		user_cookie = self.request.cookies.get('user_id')
 		user = False
-		if user_cookie:			
+		if user_cookie:
 			user = cookieStuff.check_secure_val(user_cookie)
-		if not user and redirect:			
+		if not user and redirect:
 			self.redirect("/signup")
 		return user
 
+
+class imageHandler(Handler):
+	def post(self):
+		delete_image = self.request.get('delete_image')
+		blog_id = self.request.get('blog_id')
+		image = self.request.get('img')
+		
+		blog = Blogs.get_by_id(int(blog_id))
+
+
+		if blog.created_by == self.user_set(False):
+			if delete_image:
+				blog.image = None
+				blog.put()
+			else:
+				image = images.resize(image, 500,500)
+				blog.image = image
+				blog.put()
+		else:
+			pass
+			# Write error handling
+
+
+class Image(Handler):
+	def get(self):
+		blog = Blogs.get_by_id(int(self.request.get('img_id')))
+		if blog.image:
+			self.response.headers['Content-Type'] = 'image/jpeg'
+			self.write(blog.image)
+		else:
+			self.write('No Image')
+
+
 class signupHandler(Handler):
 	def get(self):
-		self.render('signup.html')
+		self.render('signup.html', user = False)
 	def post(self):
 		#self.response.headers['Content-Type'] = 'text/plain'
 		username= self.request.get('username')
@@ -53,40 +88,40 @@ class signupHandler(Handler):
 		emailError = ""
 		verifyError = ""
 		hasError = False
-			
+
 		if not Valid.username(username):
 			userError = "Username is not Valid"
 			hasError = True
-		
+
 		if not Valid.password(password):
 			passError = "Password Not Valid"
 			hasError = True
-		
+
 		if email and not Valid.email(email):
 			emailError = "Email Not Valid"
 			hasError = True
-		
+
 		if password != verify:
 			verifyError = "Passwords Do not Match"
 			hasError = True
-		
+
 		if Valid.user_exists(username):
 			userError = "Username already exists"
 			hasError = True
-		
+
 		if not hasError:
 			password = hashpass.make_pw_hash(username, password)
 			u = Users(user = username, password = password)
 			u.put()
 			value = cookieStuff.make_secure_val(username)
 			self.response.headers.add_header(
-				'Set-Cookie', 
+				'Set-Cookie',
 				str('user_id=%s; Path=/' % value))
 			self.redirect("/welcome")
 
 		self.render("signup.html", userError = userError,
 					passError = passError, emailError = emailError,
-					verifyError = verifyError)
+					verifyError = verifyError, user = False)
 
 class Blogs(db.Model):
 	subject = db.StringProperty(required = True)
@@ -94,14 +129,16 @@ class Blogs(db.Model):
 	created = db.DateTimeProperty(auto_now_add = True)
 	created_by = db.StringProperty(required = True)
 	num_likes = db.IntegerProperty(required = True)
-	
+	num_comments = db.IntegerProperty()
+	image = db.BlobProperty()
+
 class Users(db.Model):
 	user = db.StringProperty(required = True)
 	password = db.StringProperty(required = True)
 
 	@classmethod
 	def by_username(cls, username):
-		return cls.all().filter("user = ", username).get()
+		return cls.query().filter(cls.user == username).get()
 
 class Likes(db.Model):
 	user = db.StringProperty(required = True)
@@ -109,8 +146,10 @@ class Likes(db.Model):
 
 	@classmethod
 	def already_liked(cls,username,blog_id):
-		find_user_like = cls.all().filter("user = ", username)
-		find_post_like = find_user_like.filter("blog_id = ", blog_id)
+		if not username:
+			return False
+		find_user_like = cls.query().filter(cls.user == username)
+		find_post_like = find_user_like.filter(cls.blog_id == blog_id)
 		if find_post_like.get() is None:
 			return False
 		else:
@@ -120,7 +159,7 @@ class Likes(db.Model):
 	def generate_likes_list(cls,user,blogs):
 		likes_for_page = []
 		for blog in blogs:
-			if Likes.already_liked(user,blog.key().id()):
+			if Likes.already_liked(user,blog.key.id()):
 				likes_for_page.append(True)
 			else:
 				likes_for_page.append(False)
@@ -134,18 +173,20 @@ class Comments(db.Model):
 
 class bloglistHandler(Handler):
 	def get(self):
-		blogs = Blogs.all().order('-created')
+		blogs = Blogs.query().order(-Blogs.created)
 		user = self.user_set(False)
 		likes_for_page = Likes.generate_likes_list(user,blogs)
 		error = ''
 		self.render(
-			'bloglist.html', 
+			'bloglist.html',
 			blogs = blogs,
 			likes_for_page = likes_for_page,
 			error = error,
-			liked_idx = 0)
+			liked_idx = 0,
+			user = user)
+
 	def post(self):
-		blogs = Blogs.all().order('-created')
+		blogs = Blogs.query().order(-Blogs.created)
 		user = self.user_set(False)
 		liked_idx = self.request.get('blog_idx')
 		blog_id = self.request.get('blog_id')
@@ -172,17 +213,18 @@ class bloglistHandler(Handler):
 			 blogs = blogs,
 			 error = error,
 			 likes_for_page = likes_for_page,
-			 liked_idx = int(liked_idx)
-			 )
-
+			 liked_idx = int(liked_idx))
+		self.render(
+			'base.html',
+			user = user)
 
 class blogHandler(Handler):
 	def get(self, blogID):
 		blog = Blogs.get_by_id(int(blogID))
 		user = self.user_set(False)
-		comments = Comments.all().filter("parent_blog = ",int(blogID)).order("created")
+		comments = Comments.query().filter(Comments.parent_blog == int(blogID)).order(Comments.created)
 		self.render(
-			'blog.html', 
+			'blog.html',
 			blog = blog,
 			user = user,
 			comments = comments)
@@ -192,13 +234,22 @@ class blogHandler(Handler):
 		content = self.request.get('content')
 		cancel = self.request.get('cancel')
 		edit_blog = self.request.get('edit_blog')
+		user = self.user_set(False)
 		if cancel:
 			self.redirect('/%s' % blogID)
 		error = ''
-		if self.request.get('delete') and blog.created_by == self.user_set():
-			blog.delete()
+		if self.request.get('delete') and blog.created_by == user:
+
+			comments = Comments.query().filter(Comments.parent_blog == int(blogID))
+			likes = Likes.query().filter(Likes.blog_id == int(blogID))
+			for comment in comments:
+				comment.key.delete()
+			for like in likes:
+				like.key.delete()
+			blog.key.delete()
+
 			self.redirect('/')
-		if blog.created_by == self.user_set() \
+		if blog.created_by == user \
 			and subject and content:
 			blog.subject = subject
 			blog.content = content
@@ -206,21 +257,23 @@ class blogHandler(Handler):
 			error = 'You have edited your blog'
 		elif not edit_blog:
 			error = "We need both Subject and Content for each post"
-		comments = Comments.all().filter("parent_blog=",blogID)
+		comments = Comments.query().filter(Comments.parent_blog == int(blogID))
 		self.render(
-			'editBlog.html', 
+			'editBlog.html',
+			user = user,
 			blog = blog,
 			error = error
-			)	
+			)
 
 class newpostHandler(Handler):
 	def get(self):
-		self.user_set()
+		user = self.user_set()
 		self.render(
-			'newpost.html', 
-			error = '', 
+			'newpost.html',
+			error = '',
 			subject = '',
-			content = '')				
+			content = '',
+			user = user)
 
 	def post(self):
 		user = self.user_set()
@@ -229,25 +282,27 @@ class newpostHandler(Handler):
 
 		if subject and content:
 			b = Blogs(
-				subject = subject, 
-				content = content, 
+				subject = subject,
+				content = content,
 				created_by = user,
-				num_likes = 0)
+				num_likes = 0,
+				num_comments = 0)
 			b.put()
-			self.redirect('/%s'%b.key().id())
+			self.redirect('/%s'%b.key.id())
 		else:
 			error = "We need both a title and blog entry"
 			self.render(
-				'newpost.html', 
-				subject = subject, 
-				content = content, 
-				error = error)
+				'newpost.html',
+				subject = subject,
+				content = content,
+				error = error,
+				user = user)
 
 class welcomeHandler(Handler):
 	def get(self):
-		username = self.user_set()
-		self.render("welcome.html",username = username)
-		
+		user = self.user_set()
+		self.render("welcome.html",user = user)
+
 
 class loginHandler(Handler):
 	def get(self):
@@ -256,19 +311,19 @@ class loginHandler(Handler):
 		username = self.request.get('username')
 		password = self.request.get('password')
 		user = Valid.user_exists(username)
-		if user and hashpass.valid_pw(username, password, user.password):	
+		if user and hashpass.valid_pw(username, password, user.password):
 			value = cookieStuff.make_secure_val(username)
 			self.response.headers.add_header('Set-Cookie',
 							str('user_id=%s; Path=/' % value))
-			self.redirect("/welcome")			
+			self.redirect("/welcome")
 		else:
 			loginError = "Incorrect Login"
 			self.render("login.html", loginError = loginError)
-			
+
 class logoutHandler(Handler):
 	def get(self):
 		self.response.headers.add_header(
-			'Set-Cookie', 
+			'Set-Cookie',
 			str('user_id=; Path=/'))
 		self.redirect("/signup")
 
@@ -280,18 +335,18 @@ class Valid():
 	@classmethod
 	def username(self, username):
 		return self.USER_RE.match(username)
-	
-	@classmethod	
+
+	@classmethod
 	def password(self, password):
 		return self.PASS_RE.match(password)
-	
-	@classmethod	
+
+	@classmethod
 	def email(self, email):
 		return self.EMAIL_RE.match(email)
-	
+
 	@classmethod
 	def user_exists(self, username):
-		user = db.Query(Users).filter('user =', username).get()
+		user = Users.query().filter(Users.user == username).get()
 		if user:
 			return user
 		else:
@@ -313,20 +368,21 @@ class spamHandler(Handler):
 	def spam_site_with_blogs(self,blogs = 10):
 		for i in range(1,blogs):
 			blog = Blogs(
-				subject = "fuck you", 
+				subject = "fuck you",
 				content = "I hate you!",
 				created_by = "Travis",
-				num_likes = 0)
+				num_likes = 0,
+				num_comments = 0)
 			blog.put()
-	
+
 	def delete_all_blogs(self):
-		query = Blogs.all()
+		query = Blogs.query()
 		for blog in query:
-			blog.delete()
+			blog.key.delete()
 
 class likesHandler(Handler):
 	def post(self):
-		blogs = Blogs.all().order('-created')
+		blogs = Blogs.query().order(-Blogs.created)
 		user = self.user_set(False)
 		liked_idx = self.request.get('blog_idx')
 		blog_id = self.request.get('blog_id')
@@ -339,7 +395,7 @@ class likesHandler(Handler):
 			error = 'You must be signed in to like someones post!'
 		elif liked:
 			error = 'You have unliked this post!'
-			liked.get().delete()
+			liked.get().key.delete()
 			liked_blog.num_likes -= 1
 			liked_blog.put()
 			net_like = -1
@@ -358,7 +414,7 @@ class likesHandler(Handler):
 			'num_likes': liked_blog.num_likes
 		}
 		self.write(json.dumps(json_response))
-		
+
 
 class commentsHandler(Handler):
 	def post(self):
@@ -367,46 +423,71 @@ class commentsHandler(Handler):
 		user = self.user_set(False)
 		delete_comment = self.request.get('delete_comment')
 		comment_id = self.request.get('comment_id')
+		update_comment = self.request.get('update_comment')
+		add_comment = self.request.get('add_comment')
+
+
 		if delete_comment:
 			comment = Comments.get_by_id(int(comment_id))
 			blog = Blogs.get_by_id(int(blog_id))
 			if user == comment.user or user == blog.created_by:
-				comment.delete()
+				comment.key.delete()
+				blog.num_comments -= 1
+				blog.put()
 				return
-			
+
+		if update_comment:
+			comment = Comments.get_by_id(int(comment_id))
+			if comment and user == comment.user and comment_text:
+				comment.content = comment_text
+				comment.put()
+				json_response = {
+					'error': 'Thanks for updating the comment!',
+					'user': user,
+					'ret_val': True,
+					'comment_content': comment_text,
+					'comment_id': comment.key.id()
+				}
+				self.write(json.dumps(json_response))
+			else:
+				json_response = {
+					'error': 'There was an error!',
+					'ret_val': False,
+				}
+				self.write(json.dumps(json_response))
+
+
+
 
 		if not user:
 			error = 'You must be signed in to comment on a blog!'
 			ret_val = 0
-
-		if user and comment_text:
-			c = Comments(content = comment_text, user = user, parent_blog = int(blog_id))
-			c.put()
-			json_response = {
-			'error': 'Thanks for adding a comment!',
-			'user': user,
-			'ret_val': True,
-			'comment_id': c.key().id()
-			}
-			self.write(json.dumps(json_response))
-		elif not user:
-			json_response = {
-			'error': 'You must be signed in to add a comment',
-			'ret_val': False
-			}
-			self.write(json.dumps(json_response))
-		elif not comment_text:
-			json_response = {
-			'error': 'You cannot add a blank comment',
-			'ret_val': False
-			}
-			self.write(json.dumps(json_response))
-
-
-			
-
-
-
+		if add_comment:
+			if user and comment_text:
+				blog = Blogs.get_by_id(int(blog_id))
+				c = Comments(content = comment_text, user = user, parent_blog = int(blog_id))
+				c.put()
+				blog.num_comments += 1
+				blog.put()
+				json_response = {
+					'error': 'Thanks for adding a comment!',
+					'user': user,
+					'ret_val': True,
+					'comment_id': c.key.id()
+				}
+				self.write(json.dumps(json_response))
+			elif not user:
+				json_response = {
+				'error': 'You must be signed in to add a comment',
+				'ret_val': False
+				}
+				self.write(json.dumps(json_response))
+			elif not comment_text:
+				json_response = {
+				'error': 'You cannot add a blank comment',
+				'ret_val': False
+				}
+				self.write(json.dumps(json_response))
 
 app = webapp2.WSGIApplication([
 		('/', bloglistHandler),
@@ -419,4 +500,6 @@ app = webapp2.WSGIApplication([
 		('/spam', spamHandler),
 		('/likes', likesHandler),
 		('/comments', commentsHandler),
+		('/img', Image),
+		('/images', imageHandler),
 	], debug=True)
